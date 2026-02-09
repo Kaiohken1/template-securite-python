145f38379ae5c298524e41c2dfc750a0204a50ef
Kaioken <raygsm@live.fr>
2026-02-09 13:43:56 +0100

    add captcha test

--- tests/tp3/utils/test_captcha.py
+++ tests/tp3/utils/test_captcha.py
@@ -1,12 +1,16 @@
 from src.tp3.utils.captcha import Captcha
+from PIL import Image
+from requests import Session
+
 
 
 def test_captcha_init():
     # Given
     url = "http://example.com/captcha"
+    session = Session()
 
     # When
-    captcha = Captcha(url)
+    captcha = Captcha(url, session)
 
     # Then
     assert captcha.url == url
@@ -16,30 +20,36 @@ def test_captcha_init():
 
 def test_solve():
     # Given
-    captcha = Captcha("http://example.com/captcha")
+    session = Session()
+
+    captcha = Captcha("http://example.com/captcha", session)
+
+    captcha.image = Image.open('captcha_test.png')
 
     # When
     captcha.solve()
 
     # Then
-    assert captcha.value == "FIXME"
+    assert captcha.value == "758841"
 
 
 def test_capture():
     # Given
-    captcha = Captcha("http://example.com/captcha")
+    session = Session()
+    captcha = Captcha("http://31.220.95.27:9002/captcha.php", session)
 
     # When
     captcha.capture()
 
     # Then
-    # This is a minimal test since the method doesn't do anything yet
-    assert captcha.image == ""
+    assert len(captcha.image) > 0
+    assert len(captcha.session) > 0
 
 
 def test_get_value():
     # Given
-    captcha = Captcha("http://example.com/captcha")
+    session = Session()
+    captcha = Captcha("http://example.com/captcha", session)
     captcha.value = "TEST123"
 
     # When
